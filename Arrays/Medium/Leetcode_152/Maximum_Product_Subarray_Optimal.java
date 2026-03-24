eebad2a3e94ac8f8db41f7e17ca5e615e981c690
Srushti Sambare <srushtisambare@gmail.com>
2026-03-24 20:27:55 +0530

    Create Maximum_Product_Subarray_Optimal.java

---
+++ Arrays/Medium/Leetcode_152/Maximum_Product_Subarray_Optimal.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+
+        int prefix = 1;
+        int suffix = 1;
+        int maxProd = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++){
+
+            if(prefix == 0){
+                prefix = 1;
+            }
+
+            if(suffix == 0){
+                suffix = 1;
+            }
+
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n-i-1];
+
+            maxProd = Math.max(maxProd, Math.max(prefix, suffix));
+        }
+
+        return maxProd;
+    }
+}
