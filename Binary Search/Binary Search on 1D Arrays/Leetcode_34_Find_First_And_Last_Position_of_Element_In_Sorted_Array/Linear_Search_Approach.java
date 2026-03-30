29668fe2e89fb33150aa0835fe14bafed6f1305c
Srushti Sambare <srushtisambare@gmail.com>
2026-03-30 11:46:15 +0530

    Add linear search approach for finding target range
    Implement linear search to find the first and last positions of a target in a sorted array.

---
+++ Binary Search/Binary Search on 1D Arrays/Leetcode_34_Find_First_And_Last_Position_of_Element_In_Sorted_Array/Linear_Search_Approach.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        
+        int first = -1;
+        int last = -1;
+
+        for(int i=0; i<n; i++){
+            if(nums[i] == target){
+                if(first==-1){
+                    first = i;
+                    last = i;
+                }
+                else{
+                    last = i;
+                }
+            }
+        }
+
+        return new int[]{first,last};
+    }
+}
