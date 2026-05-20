8f3eb235534a672eb170f2704891eb8a4eb233d3
Srushti Sambare <srushtisambare@gmail.com>
2026-05-20 11:27:51 +0530

    Implement solution for largest odd number in string

---
+++ Strings/Leetcode_1903_Largest Odd Number in String/Greedy.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String largestOddNumber(String num) {
+        
+        int n = num.length();
+
+        int index = -1;
+
+        for(int i=n-1; i>=0; i--){
+
+            int digit = num.charAt(i) - '0';
+
+            if(digit % 2 == 1){
+                index = i;
+                break;
+            }
+        }
+
+        if(index == -1){
+            return "";
+        }
+
+        return num.substring(0, index+1);
+    }
+}
